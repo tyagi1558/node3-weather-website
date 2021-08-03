0688d458ccac7f5554cf7887308457deda30cea3
Anmol Tyagi <72148926+tyagi1558@users.noreply.github.com>
2021-08-03 14:33:50 +0530

    Init commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "abde4a5d978447afebc860ec8818d62e"
+    }
+}
+
+module.exports = constants;
